64f54739c8516d630c23f6bf8d80e47b9d361107
Johan Roelofsen <j.roelofsen@bigmile.eu>
2022-08-14 15:02:46 +0200

    Add dataframe melt doc example

--- polars/dataframe.ts
+++ polars/dataframe.ts
@@ -916,7 +916,29 @@ export interface DataFrame
    * ___
    *
    * @param idVars - Columns to use as identifier variables.
-   * @param valueVars - Values to use as identifier variables.
+   * @param valueVars - Values to use as value variables.
+   * @example
+   * ```
+   * >>> df1 = pl.DataFrame({
+   * >>>   'id': [1],
+   * >>>   'asset_key_1': ['123'],
+   * >>>   'asset_key_2': ['456'],
+   * >>>   'asset_key_3': ['abc'],
+   * >>> })
+   * >>> df1.melt('id', ['asset_key_1', 'asset_key_2', 'asset_key_3'])
+   * shape: (3, 3)
+   * ┌─────┬─────────────┬───────┐
+   * │ id  ┆ variable    ┆ value │
+   * │ --- ┆ ---         ┆ ---   │
+   * │ f64 ┆ str         ┆ str   │
+   * ╞═════╪═════════════╪═══════╡
+   * │ 1   ┆ asset_key_1 ┆ 123   │
+   * ├╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌┤
+   * │ 1   ┆ asset_key_2 ┆ 456   │
+   * ├╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌┤
+   * │ 1   ┆ asset_key_3 ┆ abc   │
+   * └─────┴─────────────┴───────┘
+   * ```
    */
   melt(idVars: ColumnSelection, valueVars: ColumnSelection): DataFrame;
   /**
@@ -1814,8 +1836,7 @@ export const _DataFrame = (_df: any): DataFrame => {
       return wrap("extend", (other as any).inner());
     },
     filter(predicate) {
-      return this.lazy().filter(predicate)
-        .collectSync();
+      return this.lazy().filter(predicate).collectSync();
     },
     fillNull(strategy) {
       return wrap("fillNull", strategy);
@@ -2048,9 +2069,7 @@ export const _DataFrame = (_df: any): DataFrame => {
     select(...selection) {
       const hasExpr = selection.flat().some((s) => Expr.isExpr(s));
       if (hasExpr) {
-        return _DataFrame(_df).lazy()
-          .select(selection)
-          .collectSync();
+        return _DataFrame(_df).lazy().select(selection).collectSync();
       } else {
         return wrap("select", columnOrColumnsStrict(selection as any));
       }
