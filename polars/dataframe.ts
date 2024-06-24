6d508697be4f58dd12254c1feb6a01b924cdd59f
lbardalezg <lbardalezg@gmail.com>
2024-06-24 09:38:14 -0500

    fix: fixing incorrect example (#231)
    "value:" is incorrect in the context of pivot, removed

--- polars/dataframe.ts
+++ polars/dataframe.ts
@@ -1034,7 +1034,7 @@ export interface DataFrame
    *   ...         "baz": [1, 2, 3, 4, 5, 6],
    *   ...     }
    *   ... );
-   *   > df.pivot(values:"baz", {index:"foo", columns:"bar"});
+   *   > df.pivot("baz", {index:"foo", columns:"bar"});
    *   shape: (2, 4)
    *   ┌─────┬─────┬─────┬─────┐
    *   │ foo ┆ A   ┆ B   ┆ C   │
