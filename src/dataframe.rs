5f73396243378e681ef1a691b6476d2b11ac412b
Bidek56 <bidek56@users.noreply.github.com>
2025-11-05 14:20:04 -0500

    Fixing unnest

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -1197,8 +1197,9 @@ impl JsDataFrame {
         s.into_series().into()
     }
     #[napi(catch_unwind)]
-    pub fn unnest(&self, names: Vec<String>) -> napi::Result<JsDataFrame> {
-        let df = self.df.unnest(names, Some(&PlSmallStr::EMPTY)).map_err(JsPolarsErr::from)?;
+    pub fn unnest(&self, names: Vec<String>, separator: Option<String>) -> napi::Result<JsDataFrame> {
+        let separator = separator.map_or(None, |s| Some(s));
+        let df = self.df.unnest(names, separator.as_deref()).map_err(JsPolarsErr::from)?;
         Ok(df.into())
     }
     #[napi(catch_unwind)]
