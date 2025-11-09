12170640b18d1a13d76c448ac1f66a917b8e942e
Bidek56 <bidek56@users.noreply.github.com>
2025-11-09 18:40:44 -0500

    Adding duration test

--- __tests__/series.test.ts
+++ __tests__/series.test.ts
@@ -782,6 +782,14 @@ describe("series functions", () => {
   });
 });
 describe("comparators & math", () => {
+  test("duration/add/series", () => {
+    const drs = pl.Series("dur", [1], pl.Duration("ms"));
+    const dt = new Date(Date.now());
+    const ds = pl.Series("dt", [dt], pl.Datetime("ms", ""));
+    const expected = dt.getMilliseconds() + 1;
+    const actual = ds.add(drs).values().next().value.getMilliseconds();
+    expect(actual).toEqual(expected);
+  });
   test("add/plus/series", () => {
     const s = pl.Series([1, 2, 3]);
     const expected = pl.Series([2, 4, 6]);
